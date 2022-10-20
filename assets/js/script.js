688bbc515261450375d0625dc005fb26b4631339
CtrlAltRee <dess.rucker@gmail.com>
2022-10-19 23:17:13 -0400

    add scale up on mouse enter for artwork

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
+//cursor effect
 //get cursor
 const cursor = document.querySelector('.cursor');
 const cursorTrail = document.querySelector('.cursor-trail');
 
 // console.log(cursor);
 // when user moves mouse, set position of horizontal and vertical coordinates
-document.addEventListener("mousemove", (e) => {
+document.addEventListener('mousemove', (e) => {
   cursor.style.left = e.clientX + "px";
   cursor.style.top = e.clientY + "px";
   setTimeout(() => {
@@ -13,7 +14,7 @@ document.addEventListener("mousemove", (e) => {
   }, 270);
 });
 
-//scroll to top 
+//scroll to top button
 //get button
 const scrollButton = document.getElementById('btn-scroll');
 
@@ -36,4 +37,20 @@ scrollButton.addEventListener('click', backToTop);
 function backToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-}
+};
+
+//artwork 
+//get artwork
+
+let artworkImg = document.querySelectorAll('.artwork-img');
+
+//when user mouse points to art, art scales up, mouse leaves art, art returns to size
+artworkImg.forEach(function(element) {
+    element.addEventListener('mouseenter', function() {
+        this.classList.add('artpics');
+    });
+    element.addEventListener('mouseleave', function() {
+        this.classList.remove('artpics');
+    });
+});
+
